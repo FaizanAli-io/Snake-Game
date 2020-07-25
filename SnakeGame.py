a6f925e53dfadc4a6c93300d70afb19c8c8e68fe
Faizan Ali <fali.abdulali@gmail.com>
2020-07-25 10:29:17 +0500

    new method

--- SnakeGame.py
+++ SnakeGame.py
@@ -9,9 +9,9 @@ pygame.display.set_caption("Connect 4")
 pygame.mouse.set_cursor(*pygame.cursors.diamond)
 clock = pygame.time.Clock()
 foodpics = [pygame.image.load('apple.png'), pygame.image.load('orange.png'), pygame.image.load('mango.png'), pygame.image.load('banana.png')]
-play, autoplay, paused = True, False, True
+play, autoplay, paused = True, True, False
 speed, activespeed = 10, 1
-autoplaybox, speedbox0, speedbox1, speedbox2, speedbox3 = pygame.Rect(30, 10, 160, 30), pygame.Rect(730, 10, 50, 30), pygame.Rect(790, 10, 40, 30), pygame.Rect(840, 10, 40, 30), pygame.Rect(890, 10, 40, 30)
+autoplaybox, speedbox0, speedbox1, speedbox2, speedbox3 = pygame.Rect(30, 10, 160, 30), pygame.Rect(720, 10, 50, 30), pygame.Rect(780, 10, 40, 30), pygame.Rect(830, 10, 40, 30), pygame.Rect(880, 10, 50, 30)
 font = pygame.font.SysFont('inkfree', 20, 2)
 textposx, textposy = 40, 12
 food = random.choice(foodpics)
@@ -40,7 +40,7 @@ def buttons(pos = None):
     else:
         pygame.draw.rect(screen, red, speedbox0, 2)
         text = font.render('0.5x', True, red)
-    screen.blit(text, (textposx+695, textposy))
+    screen.blit(text, (textposx+685, textposy))
 
     if activespeed == 1:
         pygame.draw.rect(screen, green, speedbox1, 2)
@@ -49,7 +49,7 @@ def buttons(pos = None):
     else:
         pygame.draw.rect(screen, red, speedbox1, 2)
         text = font.render('1x', True, red)
-    screen.blit(text, (textposx+760, textposy))
+    screen.blit(text, (textposx+750, textposy))
     
     if activespeed == 2:
         pygame.draw.rect(screen, green, speedbox2, 2)
@@ -58,22 +58,22 @@ def buttons(pos = None):
     else:
         pygame.draw.rect(screen, red, speedbox2, 2)
         text = font.render('2x', True, red)
-    screen.blit(text, (textposx+805, textposy))
+    screen.blit(text, (textposx+795, textposy))
     
     if activespeed == 3:
         pygame.draw.rect(screen, green, speedbox3, 2)
-        text = font.render('4x', True, green)
-        speed = 40
+        text = font.render('test', True, green)
+        speed = 200
     else:
         pygame.draw.rect(screen, red, speedbox3, 2)
-        text = font.render('4x', True, red)
-    screen.blit(text, (textposx+855, textposy))
+        text = font.render('test', True, red)
+    screen.blit(text, (textposx+843, textposy))
 
-def game_over():
+def game_over(win = False):
     global play
     play = False
-    if len(mysnake.body) != 15*25: messagebox.showinfo("Game Over", "You did your best mate, GG!")
-    else: messagebox.showinfo("You Win", "Congrats! You won this shitty version of Snake!")
+    if not win: messagebox.showinfo("Game Over", "You did your best mate, GG!\nYour score is {}".format((len(mysnake.body)-1)*10))
+    if win: messagebox.showinfo("Game Over", "You won, GG!")
 
 class Segment:
     def __init__(self, i, j, nex = None):
@@ -81,19 +81,12 @@ class Segment:
         self.j = j
         self.next = nex
 
-    def __str__(self):
-        if self.next == None: return 'head'
-        return str((self.i, self.j))
-
 class Snake:
     def __init__(self):
-        self.body = [Segment(7, 12)]
+        self.body = [Segment(0, 0)]
         self.direction = 'down'
-        self.a = set([(i, j) for j in range(25) for i in range(15)])
+        self.a = set([(i, j) for j in range(col) for i in range(row)])
         self.food = random.choice(list(self.a))
-
-    def __repr__(self):
-        return str([(seg.i, seg.j) for seg in self.body])
 
     def grow(self):
         new = Segment(self.body[-1].i, self.body[-1].j, self.body[-1])
@@ -119,8 +112,12 @@ class Snake:
         screen.blit(food, loc)
         if self.food == (self.body[0].i, self.body[0].j):
             b = set([(seg.i, seg.j) for seg in self.body])
-            self.a -= b
-            self.food = random.choice(list(self.a))
+            c = self.a.copy()
+            c -= b
+            try:
+                self.food = random.choice(list(c))
+            except:
+                game_over(True)
             self.grow()
             food = random.choice(foodpics)
 
@@ -130,12 +127,12 @@ class Snake:
             if (seg.i, seg.j) == (i, j):
                 game_over()
                 return
-        if i < 0 or i > 14 or j < 0 or j > 24:
+        if i < 0 or i > (row - 1) or j < 0 or j > (col - 1):
             game_over()
             return
 
     def blocked(self, i, j):
-        if i < 0 or i > 14 or j < 0 or j > 24: return True
+        if i < 0 or i > (row - 1) or j < 0 or j > (col - 1): return True
         for seg in self.body:
             if (i, j) == (seg.i, seg.j): return True
         return False
@@ -151,6 +148,30 @@ class Snake:
         if moves:
             idx = min(moves, key=lambda x: moves[x])
             self.direction = idx
+
+    def direct2(self):
+        i, j = self.body[0].i, self.body[0].j
+        if i == 0:
+            if j == col - 1:
+                self.direction = 'down'
+                return
+            self.direction = 'right'
+            return
+        
+        if (j % 2 == 1 and i == row - 1) or (j % 2 == 0 and i == 1):
+            if i == 1 and j == 0:
+                self.direction = 'up'
+                return
+            self.direction = 'left'
+            return
+        
+        if j % 2 == 0:
+            self.direction = 'up'
+            return
+
+        if j % 2 == 1:
+            self.direction = 'down'
+            return
 
 class Box:
     def __init__(self, i, j, buffX, buffY, blocksize):
@@ -180,34 +201,42 @@ class Grid:
         buttons()
         if not paused: self.snake.move()
         else: screen.blit(font.render('Paused', True, red), (500, 12))
-        [pygame.draw.rect(screen, white, self.boxes[seg.i][seg.j].position) for seg in self.snake.body]            
+        pygame.draw.rect(screen, white, self.boxes[self.snake.body[0].i][self.snake.body[0].j].position, 2)
+        [pygame.draw.rect(screen, white, self.boxes[seg.i][seg.j].position) for seg in self.snake.body[1:]]            
 
+row, col = 15, 26
 mysnake = Snake()
-mygrid = Grid(15, 25, mysnake)
+mygrid = Grid(row, col, mysnake)
 
-while play:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE: sys.exit()
-            if event.key == pygame.K_p: paused = True if not paused else False
-            if event.key == pygame.K_SPACE: autoplay = True if not autoplay else False
-            if event.key == pygame.K_0: activespeed = 0
-            if event.key == pygame.K_1: activespeed = 1
-            if event.key == pygame.K_2: activespeed = 2
-            if event.key == pygame.K_3: activespeed = 3
-            if not autoplay:
-                if (event.key == pygame.K_DOWN or event.key == pygame.K_s) and mysnake.direction != 'up': mysnake.direction = 'down'
-                if (event.key == pygame.K_UP or event.key == pygame.K_w) and mysnake.direction != 'down': mysnake.direction = 'up'
-                if (event.key == pygame.K_RIGHT or event.key == pygame.K_d) and mysnake.direction != 'left': mysnake.direction = 'right'
-                if (event.key == pygame.K_LEFT or event.key == pygame.K_a) and mysnake.direction != 'right': mysnake.direction = 'left'
-        if event.type == pygame.MOUSEBUTTONDOWN: buttons(pygame.mouse.get_pos())
-    
-    if autoplay: mysnake.direct()
+def game_loop():
+    global mygrid, mysnake, activespeed, autoplay, paused
+    algo = mysnake.direct
 
-    screen.fill(black)
-    mygrid.show()
-    pygame.display.flip()
-    clock.tick(speed)
+    while play:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE: sys.exit()
+                if event.key == pygame.K_p: paused = True if not paused else False
+                if event.key == pygame.K_SPACE: autoplay = True if not autoplay else False
+                if event.key == pygame.K_0: activespeed = 0
+                if event.key == pygame.K_1: activespeed = 1
+                if event.key == pygame.K_2: activespeed = 2
+                if event.key == pygame.K_3: activespeed = 3
+                if event.key == pygame.K_m: algo = mysnake.direct if algo == mysnake.direct2 else mysnake.direct2
+                if not autoplay:
+                    if (event.key == pygame.K_DOWN or event.key == pygame.K_s) and mysnake.direction != 'up': mysnake.direction = 'down'
+                    if (event.key == pygame.K_UP or event.key == pygame.K_w) and mysnake.direction != 'down': mysnake.direction = 'up'
+                    if (event.key == pygame.K_RIGHT or event.key == pygame.K_d) and mysnake.direction != 'left': mysnake.direction = 'right'
+                    if (event.key == pygame.K_LEFT or event.key == pygame.K_a) and mysnake.direction != 'right': mysnake.direction = 'left'
+            if event.type == pygame.MOUSEBUTTONDOWN: buttons(pygame.mouse.get_pos())
+        
+        if autoplay: algo()
 
+        screen.fill(black)
+        mygrid.show()
+        pygame.display.flip()
+        clock.tick(speed)
+
+game_loop()
 sys.exit()
